b0368a67fd3f6f0e77899d04481705ea895ead84
bob_kubinec <rmk7xy@virginia.edu>
2016-10-11 09:15:12 -0400

    adding a package library to manage data cleaning

--- Run_model_newdata.R
+++ Run_model_newdata.R
@@ -18,7 +18,7 @@ use_subset <- TRUE
 subset_party <- c("Bloc Al Horra","Mouvement Nidaa Tounes")
 use_both <- FALSE
 # Which of the legislatures to use-- ARP or ANC
-legislature <- "ARP"
+legislature <- "arp_votes"
 # Use variational inference? Faster, but less accurate
 use_vb <- FALSE
 # Convert absences to a separate category in ordinal regression?
@@ -28,7 +28,7 @@ to_run <- 3
 # Use only a sample of bills/legislators?
 sample_it <- FALSE
 # Legislator to use as a reference for IRT model
-refleg <- "ARP_Bochra Belhaj Hamida"
+legislator<- "Bochra Belhaj Hamida"
 
 # clean and load data for the kind of analysis (ordinal v. binary)
 
@@ -38,97 +38,27 @@ refleg <- "ARP_Bochra Belhaj Hamida"
 # Use Bouchra's votes to filter out those particular bills that are useful for pinning the scale
 
 cleaned <- clean_data(keep_legis=keep_legis,use_subset=use_subset,subset_party=subset_party,
-                    use_both=use_both,refleg=refleg,
+                    use_both=use_both,refleg=legislator,
                     legis=1,use_vb=use_vb,use_nas=use_nas,to_run=to_run,sample_it=sample_it)
 
+to_fix  <- fix_bills(legislator=legislator,party=c("Bloc Al Horra","Mouvement Nidaa Tounes"),
+                     vote_data=cleaned,legislature="arp_votes")
 
+# Prepare matrix for model 
 
-output  <- fix_bills(legislator="Bochra Belhaj Hamida",party=c("Bloc Al Horra","Mouvement Nidaa Tounes"),
-                     party_data=combined_members,vote_data=combined_ordinal,legislature="arp_votes")
-
-
-
-
-
-
-# Now do more data processing 
-
-
-
-num_votes <- lapply(all_matrices,apply,1,function(x) {
-  y <- sum(!is.na(x))
-})
-
-bills_agree <- lapply(all_matrices,apply,2,function(x) {
-  # Should be at least two types of votes per bill
-  y <- if(length(table(x))<2) {
-    FALSE
-  } else {
-    TRUE
-  }
-  y
-})
-
-revise_data <- function(x,to_keep) {
-  to_change <- all_matrices[[x]]
-  to_change <- to_change[,bills_agree[[x]]]
-  to_change <- to_change[num_votes[[x]]>to_keep,]
-  to_change
-  }
-all_matrices <- lapply(1:length(all_matrices),revise_data,to_keep=keep_legis)
-
-if(use_both==FALSE) {
-  all_matrices <- lapply(all_matrices,function(x) x[grepl(legislature,row.names(x)),])
-}
-
-
-
+vote_matrix <- prepare_matrix(cleaned=cleaned,legislature="arp_votes",to_fix=to_fix)
 
 # Number of legislators/bills in model
-num_legis <- nrow(all_matrices[[to_run]])
-num_bills <- ncol(all_matrices[[to_run]])
+num_legis <- nrow(vote_matrix)
+num_bills <- ncol(vote_matrix)
 legislator_points <- rep(1:num_legis,times=num_bills)
 bill_points <- rep(1:num_bills,each=num_legis)
 
-# Create ideal point cutoffs
-bill_ideal <- rnorm(1000,1)
-legis_ideal <- rnorm(1000,1)
-raw_scores <- dnorm(bill_ideal,legis_ideal,0.5,log=TRUE)
-cuts <- quantile(raw_scores,probs = c(0.25,0.5,0.75))
-cut_breaks <- cuts[2:3] - cuts[1:2]
 #What to fix final bill at
 
-#First, select only those bills with lowest X-sq scores (i.e., equal proportions among categories)
-
-# x_sqs <- apply(all_matrices[[to_run]],2,function(x) {
-#   if(length(unique(x[!is.na(x)])) > 2) {
-#     value <-   table(x) %>% chisq.test()
-#     return(value$statistic) } else {
-#       return(NA)
-#     }
-#   
-# })
-# upper_bound <- quantile(x_sqs,probs=.05,na.rm=TRUE)
-# move_obs <- which(x_sqs<upper_bound & !is.na(x_sqs))
-# 
-all_matrices[[to_run]] <- cbind2(all_matrices[[to_run]][,-final_constraint],all_matrices[[to_run]][,final_constraint])
 
 
-# if(use_nas==TRUE) {
-# bill_pos <- sapply(all_matrices[[to_run]][num_legis,(num_bills-1):num_bills],switch,
-#                    -1,
-#                    -0.5,
-#                    0.5,
-#                    1)
-# } else {
-#   bill_pos <- sapply(all_matrices[[to_run]][num_legis,(num_bills-1):num_bills],switch,
-#                      -1,
-#                      0,
-#                      1)
-# }
-# bill_pos <- bill_pos[(!is.na(bill_pos) | !is.null(bill_pos))]
-
-Y <- c(all_matrices[[to_run]])
+Y <- c(vote_matrix)
 
 #Remove NAs
 remove_nas <- !is.na(Y)
