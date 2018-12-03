16b5b72da4a1ea02dae2527913b854643dd11ab5
saudiwin <bobkubinec@gmail.com>
2018-12-03 16:39:32 -0500

    i have all the predictions done!

--- R_Scripts/bawsala_predict.R
+++ R_Scripts/bawsala_predict.R
@@ -1,6 +1,7 @@
 # load fitted models, plot & analyze data
 
-require(idealstan)
+#require(idealstan)
+library(idealstan, lib.loc = "C:/Program Files/R/R-3.5.1/library")
 require(bayesplot)
 require(dplyr)
 require(tidyr)
@@ -20,3 +21,5 @@ comb_data <- readRDS('data/combine_sessions.rds')
 # see how long it takes to do predictions
 
 all_pred <- id_post_pred(group2_rw)
+
+saveRDS(all_pred,'data/all_pred.rds')
