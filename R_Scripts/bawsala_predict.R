ea8d0754e5fad962e4c77ea1718c038076a18de1
saudiwin <bobkubinec@gmail.com>
2018-12-03 10:06:59 -0500

    uploading prediction file

---
+++ R_Scripts/bawsala_predict.R
@@ -0,0 +1,22 @@
+# load fitted models, plot & analyze data
+
+require(idealstan)
+require(bayesplot)
+require(dplyr)
+require(tidyr)
+require(ggplot2)
+require(lubridate)
+require(stringr)
+require(forcats)
+require(xtable)
+
+arp_ar1 <- readRDS('data/estimate_all_ar1_vb.rds')
+arp_rw <- readRDS('data/estimate_all_rw_vb.rds')
+group2_ar1 <- readRDS('data/estimate_all_2groups_ar_vb.rds')
+group2_rw <- readRDS('data/estimate_all_2groups_rw_vb.rds')
+
+comb_data <- readRDS('data/combine_sessions.rds')
+
+# see how long it takes to do predictions
+
+all_pred <- id_post_pred(group2_rw)
