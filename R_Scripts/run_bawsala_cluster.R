d58a33c8c4d69b5e67189c1b1caa8caa1a925e6d
Robert Kubinec <bobkubinec@gmail.com>
2021-12-15 11:38:28 +0400

    most recent scripts

--- R_Scripts/run_bawsala_cluster.R
+++ R_Scripts/run_bawsala_cluster.R
@@ -32,7 +32,34 @@ all_votes <-  mutate(all_votes,mp_bloc_name=recode(mp_bloc_name,
                      vote_choice=factor(vote_choice,levels=c("NO","ABSTAIN","YES")),
                      change=as.numeric(vote_date>ymd("2016-07-13")),
                      ordered_id=3)
+
+# need to do some bill analytics  / remove bills for which we have little data
+
+bill_stat <- group_by(all_votes, bill_id) %>% 
+  count(vote_choice)
+
+# remove if yes/no < 3
+
+bill_stat_trim <- spread(bill_stat, key = "vote_choice",value="n") %>% 
+  filter(YES<3 | NO < 3)
+
+all_votes_trim <- anti_join(all_votes,bill_stat_trim,"bill_id")
+
+# check legislators
+
+leg_count <- group_by(all_votes_trim, mp_id) %>% 
+  summarize(votes=sum(vote_choice %in% c("YES","NO")))
+
+leg_trim <- filter(leg_count, votes<10)
                      
+
+all_votes_trim <- anti_join(all_votes_trim, leg_trim, by="mp_id")
+
+# collapse days into weeks
+
+all_votes_trim <- mutate(all_votes_trim,
+                         vote_month=paste0(year(vote_date),"-",month(vote_date)),
+                         vote_month=factor(vote_month))
 
 # to run this model, need to remove those parties over which the covariate is not defined
 # group_id <- filter(group_id,bloc %in% c('Nahda',
@@ -50,11 +77,11 @@ all_votes <-  mutate(all_votes,mp_bloc_name=recode(mp_bloc_name,
 
 # first run an AR(1) model with covariates
 
-arp_ideal_data <- id_make(score_data = all_votes,
+arp_ideal_data <- id_make(score_data = all_votes_trim,
                           outcome_disc="vote_choice",
                           person_id="mp_id",
                           item_id="bill_id",
-                          time_id="vote_date",
+                          time_id="vote_month",
                           group_id="mp_bloc_name",
                           person_cov=~change*mp_bloc_name)
 
@@ -64,12 +91,14 @@ estimate_all <- id_estimate(arp_ideal_data,
                             use_groups = T,
                             restrict_ind_high= "Nahda",
                             restrict_ind_low="Front Populaire",
-                            restrict_sd_low = 3,
+                            restrict_sd_low = .001,
+                            restrict_sd_high=.001,
                             fix_low = 0,
                             model_type=4,save_files="/scratch/rmk7/arp/junk/",
-                            vary_ideal_pts = 'AR1',nchains = 4,
+                            vary_ideal_pts = 'random_walk',nchains = 2,
                             ncores = parallel::detectCores(),
-                            fixtype='prefix',niters = 500,id_refresh=10)
+                            fixtype='prefix',niters = 300,warmup=300,
+                            id_refresh=10)
 
 
 
--- R_Scripts/run_bawsala_v3.R
+++ R_Scripts/run_bawsala_v3.R
@@ -1,6 +1,12 @@
 # Version 3: Using full ARP for session I/II
 # Robert Kubinec 
 
+.libPaths("/home/rmk7/other_R_libs3")
+
+require(cmdstanr)
+
+set_cmdstan_path("/home/rmk7/cmdstan")
+
 require(idealstan)
 require(bayesplot)
 require(dplyr)
@@ -13,109 +19,192 @@ require(xtable)
 
 # legislative data 
 # provided by Al-Bawsala
-all_votes <- readr::read_csv('/scratch/rmk7/arp/Votes_M01.csv')
 
-all_votes <-  mutate(all_votes,mp_bloc_name=recode(mp_bloc_name,
-                                                         `Afek Tounes et l'appel des tunisiens à l'étranger`="Afek Tounes",
-                                                         `Bloc Social-Démocrate`="Social-Démocrate",
-                                                         `Bloc Al Horra du Mouvement Machrouu Tounes`="Horra",
-                                                         `Mouvement Ennahdha`="Nahda",
-                                                         `Mouvement Nidaa Tounes`='Nidaa Tounes'),
-                     vote_title=coalesce(vote_title,as.character(vote_date)),
-                     bill_id=fct_anon(fct_cross(vote_title,law_title)),
-                     vote_choice=factor(vote_choice,levels=c("NO","ABSTAIN","YES")),
-                     change=as.numeric(vote_date>ymd("2016-07-13")))
-                     
+run_model <- F
 
-# to run this model, need to remove those parties over which the covariate is not defined
-# group_id <- filter(group_id,bloc %in% c('Nahda',
-#                                         'Nidaa Tounes',
-#                                         'Front Populaire',
-#                                         "Social-Démocrate",
-#                                         "Afek Tounes",
-#                                         "Union Patriotique Libre",
-#                                         "Alliance Dmocratique",
-#                                         "Independent"))
+if(run_model) {
+  
+  all_votes <- readr::read_csv('/scratch/rmk7/arp/Votes_M01.csv')
+  
+  all_votes <-  mutate(all_votes,mp_bloc_name=recode(mp_bloc_name,
+                                                     `Afek Tounes et l'appel des tunisiens à l'étranger`="Afek Tounes",
+                                                     `Bloc Social-Démocrate`="Social-Démocrate",
+                                                     `Bloc Al Horra du Mouvement Machrouu Tounes`="Horra",
+                                                     `Mouvement Ennahdha`="Nahda",
+                                                     `Mouvement Nidaa Tounes`='Nidaa Tounes'),
+                       vote_title=coalesce(vote_title,as.character(vote_date)),
+                       bill_id=fct_anon(fct_cross(vote_title,law_title)),
+                       vote_choice=factor(vote_choice,levels=c("NO","ABSTAIN","YES")),
+                       change=as.numeric(vote_date>ymd("2016-07-13")),
+                       ordered_id=3)
+  
+  unam_votes <- group_by(all_votes, bill_id) %>% 
+    summarize(unan=all(vote_choice[!is.na(vote_choice)]=="YES") || all(vote_choice[!is.na(vote_choice)]=="NO"))
+  
+  all_votes <- anti_join(all_votes,filter(unam_votes,unan),by="bill_id")
+  
+  # to run this model, need to remove those parties over which the covariate is not defined
+  all_votes <- filter(all_votes,mp_bloc_name %in% c('Nahda',
+                                                    'Nidaa Tounes',
+                                                    'Front Populaire',
+                                                    "Social-Démocrate",
+                                                    "Afek Tounes",
+                                                    "Union Patriotique Libre",
+                                                    "Bloc Démocrate",
+                                                    "Aucun bloc"))
+  
+  bill_stat <- all_votes %>% 
+    group_by(bill_id) %>% 
+    count(vote_choice)
+  
+  bill_stat_nahda <- filter(all_votes, mp_bloc_name=="Nahda") %>% 
+    group_by(bill_id) %>% 
+    count(vote_choice)
+  
+  bill_stat <- left_join(bill_stat,bill_stat_nahda, by=c("bill_id","vote_choice"))
+  
+  bill_sum <- group_by(bill_stat, bill_id) %>% 
+    summarize(n_yes_nahda=n.y[vote_choice=='YES']/sum(n.y,na.rm=T),
+              n_yes_total=n.x[vote_choice=='YES']/sum(n.x,na.rm=T),
+              n_no_nahda=n.y[vote_choice=='NO']/sum(n.y,na.rm=T),
+              n_no_total=n.x[vote_choice=='NO']/sum(n.x,na.rm=T)) %>% 
+    filter(!is.na(n_yes_nahda),!is.na(n_no_total))
+  
+  yes_votes <- filter(bill_sum, n_yes_nahda>.9, n_no_total>.1)
+  no_votes <- filter(bill_sum, n_no_nahda>.7, n_yes_total>.05)
+  
+  # we need to complete the data for horra and others who aren't in every time point
+  # this is for group covariate plotting
+  # all_votes <- all_votes %>% complete(law_date,nesting(bloc,change),fill=list(change=0))
+  
+  # first run an AR(1) model with covariates
+  
+  print(nrow(all_votes))
+  
+  arp_ideal_data <- id_make(score_data = all_votes,
+                            outcome_disc="vote_choice",
+                            person_id="mp_id",
+                            item_id="bill_id",
+                            time_id="vote_date",
+                            group_id="mp_bloc_name",remove_cov_int = T,
+                            person_cov=~change*mp_bloc_name)
+  
+  arp_ideal_data@person_cov <- c(arp_ideal_data@person_cov[1],arp_ideal_data@person_cov[9:15])
+  arp_ideal_data@score_matrix <- select(arp_ideal_data@score_matrix,item_id:change,
+                                        `change:mp_bloc_nameAucun bloc`:discrete)
+  
+  
+  estimate_all <- id_estimate(arp_ideal_data,
+                              use_groups = T,
+                              restrict_ind_high="3339",
+                              restrict_ind_low="0759",
+                              const_type="items",
+                              #restrict_ind_high= "Nahda",
+                              #restrict_ind_low="Front Populaire",
+                              model_type=4,save_files="/scratch/rmk7/arp/junk/",
+                              vary_ideal_pts = 'random_walk',nchains = 3,
+                              ncores = parallel::detectCores(),
+                              fixtype='prefix',niters = 500,id_refresh=10)
+  
+  
+  
+  saveRDS(estimate_all,'/scratch/rmk7/arp/estimate_all_ar1_full.rds')
+  
+} else {
+  
+  all_votes <- readr::read_csv('data/parliament_observatory_al_bawsala/Votes_M01.csv')
+  
+  all_votes <-  mutate(all_votes,mp_bloc_name=recode(mp_bloc_name,
+                                                     `Afek Tounes et l'appel des tunisiens à l'étranger`="Afek Tounes",
+                                                     `Bloc Social-Démocrate`="Social-Démocrate",
+                                                     `Bloc Al Horra du Mouvement Machrouu Tounes`="Horra",
+                                                     `Mouvement Ennahdha`="Nahda",
+                                                     `Mouvement Nidaa Tounes`='Nidaa Tounes'),
+                       vote_title=coalesce(vote_title,as.character(vote_date)),
+                       bill_id=fct_anon(fct_cross(vote_title,law_title)),
+                       vote_choice=factor(vote_choice,levels=c("NO","ABSTAIN","YES")),
+                       change=as.numeric(vote_date>ymd("2016-07-13")),
+                       ordered_id=3)
+  
+  unam_votes <- group_by(all_votes, bill_id) %>% 
+    summarize(unan=all(vote_choice[!is.na(vote_choice)]=="YES") || all(vote_choice[!is.na(vote_choice)]=="NO"))
+  
+  all_votes <- anti_join(all_votes,filter(unam_votes,unan),by="bill_id")
+  
+  # to run this model, need to remove those parties over which the covariate is not defined
+  all_votes <- filter(all_votes,mp_bloc_name %in% c('Nahda',
+                                                    'Nidaa Tounes',
+                                                    'Front Populaire',
+                                                    "Social-Démocrate",
+                                                    "Afek Tounes",
+                                                    "Union Patriotique Libre",
+                                                    "Bloc Démocrate",
+                                                    "Aucun bloc"))
+  
+  bill_stat <- all_votes %>% 
+    group_by(bill_id) %>% 
+    count(vote_choice)
+  
+  bill_stat_nahda <- filter(all_votes, mp_bloc_name=="Nahda") %>% 
+    group_by(bill_id) %>% 
+    count(vote_choice)
+  
+  bill_stat <- left_join(bill_stat,bill_stat_nahda, by=c("bill_id","vote_choice"))
+  
+  bill_sum <- group_by(bill_stat, bill_id) %>% 
+    summarize(n_yes_nahda=n.y[vote_choice=='YES']/sum(n.y,na.rm=T),
+              n_yes_total=n.x[vote_choice=='YES']/sum(n.x,na.rm=T),
+              n_no_nahda=n.y[vote_choice=='NO']/sum(n.y,na.rm=T),
+              n_no_total=n.x[vote_choice=='NO']/sum(n.x,na.rm=T)) %>% 
+    filter(!is.na(n_yes_nahda),!is.na(n_no_total))
+  
+  yes_votes <- filter(bill_sum, n_yes_nahda>.9, n_no_total>.1)
+  no_votes <- filter(bill_sum, n_no_nahda>.7, n_yes_total>.05)
+  
+  # we need to complete the data for horra and others who aren't in every time point
+  # this is for group covariate plotting
+  # all_votes <- all_votes %>% complete(law_date,nesting(bloc,change),fill=list(change=0))
+  
+  # first run an AR(1) model with covariates
+  
+  print(nrow(all_votes))
+  
+  estimate_all <- readRDS("data/estimate_all_ar1_full.rds")
+  
+  id_plot_legis_dyn(estimate_all,person_plot=F,use_ci = F) +
+    geom_vline(aes(xintercept=lubridate::ymd('2016-07-30')),
+               linetype=2) +
+    # scale_y_continuous(labels=c('More\nSecular','-1.0','-0.5','0.0','0.5','More\nIslamist'),
+    #                    breaks=c(-1.5,-1.0,-0.5,0.0,0.5,1.0)) +
+    facet_wrap(~group_id,scales='free_y')
+  
+  ggsave('party_over_time.png')
+  
+  id_plot_cov(estimate_all)
+  id_plot_cov(estimate_all,filter_cov = c('change:blocHorra','change'))
+  id_plot_legis_var(estimate_all)
+  
+  # pull out bill discrimination parameters 
+  
+  all_params <- summary(estimate_all)
+  just_discrim <- filter(all_params,grepl(pattern = 'sigma_reg_free',x=parameters)) %>% 
+    mutate(abs_score=abs(posterior_median),
+           index=as.numeric(str_extract(parameters,'[0-9]+'))) %>% 
+    arrange(desc(abs_score))
+  group_ids <- select(estimate_all@score_data@score_matrix,item_id) %>% 
+    mutate(index=as.numeric(item_id)) %>% 
+    distinct
+  
+  just_discrim <- left_join(just_discrim,group_ids,'index')
+  
+  all_out <- xtable(select(just_discrim,
+                           Vote='item_id',
+                           `Discrimination Score`="posterior_median",
+                           `Standard Deviation (Error)`="posterior_sd"))
+  print(all_out,type='latex',file='discrim_bill.tex')
+  
+  
+  
+}
 
-# we need to complete the data for horra and others who aren't in every time point
-# this is for group covariate plotting
-# all_votes <- all_votes %>% complete(law_date,nesting(bloc,change),fill=list(change=0))
-
-# first run an AR(1) model with covariates
-
-arp_ideal_data <- id_make(score_data = all_votes,
-                          outcome_disc="vote_choice",
-                          person_id="mp_id",
-                          item_id="bill_id",
-                          time_id="vote_date",
-                          group_id="mp_bloc_name",
-                          person_cov=~change*mp_bloc_name)
-
-
-
-estimate_all <- id_estimate(arp_ideal_data,
-                            use_groups = T,
-                            restrict_ind_high= "Nahda",
-                            restrict_ind_low="Front Populaire",
-                            model_type=4,
-                            vary_ideal_pts = 'AR1',nchains = 2,
-                            ncores = parallel::detectCores(),
-                            fixtype='prefix',niters = 500,id_refresh=10)
-
-
-
-saveRDS(estimate_all,'/scratch/rmk7/arp/estimate_all_ar1_full.rds')
-
-id_plot_legis_dyn(estimate_all,person_plot=F,use_ci = F) +
-  geom_vline(aes(xintercept=lubridate::ymd('2016-07-30')),
-             linetype=2) +
-  # scale_y_continuous(labels=c('More\nSecular','-1.0','-0.5','0.0','0.5','More\nIslamist'),
-  #                    breaks=c(-1.5,-1.0,-0.5,0.0,0.5,1.0)) +
-  facet_wrap(~group_id,scales='free_y')
-
-ggsave('party_over_time.png')
-
-id_plot_cov(estimate_all)
-id_plot_cov(estimate_all,filter_cov = c('change:blocHorra','change'))
-id_plot_legis_var(estimate_all)
-
-arp_ideal_data <- id_make(score_data = group_id,
-                          outcome="clean_votes",
-                          person_id="legis_names",
-                          item_id="law_unique",
-                          time_id="law_date",
-                          group_id="bloc",
-                          miss_val="4")
-
-estimate_all <- id_estimate(arp_ideal_data,
-                            use_groups = T,
-                            restrict_ind_high= "Nahda",
-                            restrict_ind_low="Front Populaire",
-                            restrict_sd_low = 3,
-                            model_type=4,
-                            vary_ideal_pts = 'random_walk',
-                            fixtype='prefix',niters=1000)
-
-
-
-saveRDS(estimate_all,'data/estimate_all_rw_full.rds')
-
-# pull out bill discrimination parameters 
-
-all_params <- summary(estimate_all)
-just_discrim <- filter(all_params,grepl(pattern = 'sigma_reg_free',x=parameters)) %>% 
-  mutate(abs_score=abs(posterior_median),
-         index=as.numeric(str_extract(parameters,'[0-9]+'))) %>% 
-  arrange(desc(abs_score))
-group_ids <- select(estimate_all@score_data@score_matrix,item_id) %>% 
-  mutate(index=as.numeric(item_id)) %>% 
-  distinct
-
-just_discrim <- left_join(just_discrim,group_ids,'index')
-
-all_out <- xtable(select(just_discrim,
-                         Vote='item_id',
-                         `Discrimination Score`="posterior_median",
-                        `Standard Deviation (Error)`="posterior_sd"))
-print(all_out,type='latex',file='discrim_bill.tex')
 
