b9c8b041fcef82c65af1d0fb2d6889500430e5dd
Bob Kubinec <bobkubinec@gmail.com>
2022-01-26 13:02:43 +0400

    fix

--- R_Scripts/run_bawsala_v3.R
+++ R_Scripts/run_bawsala_v3.R
@@ -31,17 +31,17 @@ if(run_model) {
                                                      `Bloc Social-Démocrate`="Social-Démocrate",
                                                      `Bloc Al Horra du Mouvement Machrouu Tounes`="Horra",
                                                      `Mouvement Ennahdha`="Nahda",
-                                                     `Mouvement Nidaa Tounes`='Nidaa Tounes'),
+                                                     `Mouvement Nidaa Tounes`='Nidaa Tounes',
+                                                     `Coalition nationale`="Tahya Tounes"),
                        vote_title=coalesce(vote_title,as.character(vote_date)),
-                       bill_id=fct_anon(fct_cross(vote_title,law_title)),
                        vote_choice=factor(vote_choice,levels=c("NO","ABSTAIN","YES")),
                        change=as.numeric(vote_date>ymd("2016-07-13")),
                        ordered_id=3)
   
-  unam_votes <- group_by(all_votes, bill_id) %>% 
+  unam_votes <- group_by(all_votes, vote_id) %>% 
     summarize(unan=all(vote_choice[!is.na(vote_choice)]=="YES") || all(vote_choice[!is.na(vote_choice)]=="NO"))
   
-  all_votes <- anti_join(all_votes,filter(unam_votes,unan),by="bill_id")
+  all_votes <- anti_join(all_votes,filter(unam_votes,unan),by="vote_id")
   
   # to run this model, need to remove those parties over which the covariate is not defined
   all_votes <- filter(all_votes,mp_bloc_name %in% c('Nahda',
@@ -49,6 +49,8 @@ if(run_model) {
                                                     'Front Populaire',
                                                     "Social-Démocrate",
                                                     "Afek Tounes",
+                                                    "Tahya Tounes",
+                                                    "Horra",
                                                     "Union Patriotique Libre",
                                                     "Bloc Démocrate",
                                                     "Aucun bloc")) %>% 
@@ -56,24 +58,35 @@ if(run_model) {
                                `Bloc Démocrate`="Social-Démocrate"))
   
   bill_stat <- all_votes %>% 
-    group_by(bill_id) %>% 
+    group_by(vote_id) %>% 
     count(vote_choice)
   
   bill_stat_nahda <- filter(all_votes, mp_bloc_name=="Nahda") %>% 
-    group_by(bill_id) %>% 
-    count(vote_choice)
+    group_by(vote_id) %>% 
+    count(vote_choice,.drop=F)
   
-  bill_stat <- left_join(bill_stat,bill_stat_nahda, by=c("bill_id","vote_choice"))
+  bill_stat_fp <- filter(all_votes, mp_bloc_name %in% c("Nidaa Tounes", "Tahya Tounes","Horra")) %>% 
+    group_by(vote_id) %>% 
+    count(vote_choice,.drop=F)
   
-  bill_sum <- group_by(bill_stat, bill_id) %>% 
-    summarize(n_yes_nahda=n.y[vote_choice=='YES']/sum(n.y,na.rm=T),
-              n_yes_total=n.x[vote_choice=='YES']/sum(n.x,na.rm=T),
-              n_no_nahda=n.y[vote_choice=='NO']/sum(n.y,na.rm=T),
-              n_no_total=n.x[vote_choice=='NO']/sum(n.x,na.rm=T)) %>% 
-    filter(!is.na(n_yes_nahda),!is.na(n_no_total))
+  bill_stat <- left_join(bill_stat_fp,bill_stat_nahda, by=c("vote_id","vote_choice")) %>% 
+    filter(!is.na(vote_choice))
   
-  yes_votes <- filter(bill_sum, n_yes_nahda>.9, n_no_total>.1)
-  no_votes <- filter(bill_sum, n_no_nahda>.7, n_yes_total>.05)
+  bill_stat_yes <- filter(bill_stat, vote_choice=="YES") %>% 
+    mutate(vote_diff=sqrt((n.x - n.y)^2))
+  bill_stat_no <- filter(bill_stat, vote_choice=="NO") %>% 
+    mutate(vote_diff=sqrt((n.x - n.y)^2))
+  
+  
+  # no vote: approving budget for the IVD
+  # NT split with Nahda, voted with Horra & FP in favor of law
+  # Report de l'examen du budget de l'instance vérité et dignité
+  # 58528919cf44121f3e63aee5
+  
+  # yes vote: Nahda vote for an amendment to 2018 budget (passed in Dec 2017)
+  # link: https://twitter.com/AlBawsalaTN/status/939581479952289792/photo/1
+  # not entirely sure what the amendment is about
+  # vote ID 5aeae5b84f24d02328a2f1aa
   
   # we need to complete the data for horra and others who aren't in every time point
   # this is for group covariate plotting
@@ -86,7 +99,7 @@ if(run_model) {
   arp_ideal_data <- id_make(score_data = all_votes,
                             outcome_disc="vote_choice",
                             person_id="mp_id",
-                            item_id="bill_id",
+                            item_id="vote_id",
                             time_id="vote_date",
                             group_id="mp_bloc_name",remove_cov_int = T,
                             person_cov=~change*mp_bloc_name)
@@ -98,8 +111,8 @@ if(run_model) {
   
   estimate_all <- id_estimate(arp_ideal_data,
                               use_groups = T,
-                              restrict_ind_high="0250",
-                              restrict_ind_low="3959",
+                              restrict_ind_high="5aeae5b84f24d02328a2f1aa",
+                              restrict_ind_low="58528919cf44121f3e63aee5",
                               const_type="items",time_var=10,
                               #restrict_ind_high= "Nahda",
                               #restrict_ind_low="Front Populaire",
@@ -122,16 +135,14 @@ if(run_model) {
                                                      `Bloc Al Horra du Mouvement Machrouu Tounes`="Horra",
                                                      `Mouvement Ennahdha`="Nahda",
                                                      `Mouvement Nidaa Tounes`='Nidaa Tounes'),
-                       vote_title=coalesce(vote_title,as.character(vote_date)),
-                       bill_id=fct_anon(fct_cross(vote_title,law_title)),
                        vote_choice=factor(vote_choice,levels=c("NO","ABSTAIN","YES")),
                        change=as.numeric(vote_date>ymd("2016-07-13")),
                        ordered_id=3)
   
-  unam_votes <- group_by(all_votes, bill_id) %>% 
+  unam_votes <- group_by(all_votes, vote_id) %>% 
     summarize(unan=all(vote_choice[!is.na(vote_choice)]=="YES") || all(vote_choice[!is.na(vote_choice)]=="NO"))
   
-  all_votes <- anti_join(all_votes,filter(unam_votes,unan),by="bill_id")
+  all_votes <- anti_join(all_votes,filter(unam_votes,unan),by="vote_id")
   
   # to run this model, need to remove those parties over which the covariate is not defined
   all_votes <- filter(all_votes,mp_bloc_name %in% c('Nahda',
@@ -144,16 +155,16 @@ if(run_model) {
                                                     "Aucun bloc"))
   
   bill_stat <- all_votes %>% 
-    group_by(bill_id) %>% 
+    group_by(vote_id) %>% 
     count(vote_choice)
   
   bill_stat_nahda <- filter(all_votes, mp_bloc_name=="Nahda") %>% 
-    group_by(bill_id) %>% 
+    group_by(vote_id) %>% 
     count(vote_choice)
   
-  bill_stat <- left_join(bill_stat,bill_stat_nahda, by=c("bill_id","vote_choice"))
+  bill_stat <- left_join(bill_stat,bill_stat_nahda, by=c("vote_id","vote_choice"))
   
-  bill_sum <- group_by(bill_stat, bill_id) %>% 
+  bill_sum <- group_by(bill_stat, vote_id) %>% 
     summarize(n_yes_nahda=n.y[vote_choice=='YES']/sum(n.y,na.rm=T),
               n_yes_total=n.x[vote_choice=='YES']/sum(n.x,na.rm=T),
               n_no_nahda=n.y[vote_choice=='NO']/sum(n.y,na.rm=T),
