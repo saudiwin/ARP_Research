cee07e75ef4c4c4ea010588c12ab9fc154ec0ec8
Robert Kubinec <bobkubinec@gmail.com>
2024-04-16 15:18:04 +0400

    update scripts

--- R_Scripts/run_bawsala_v3.R
+++ R_Scripts/run_bawsala_v3.R
@@ -21,63 +21,61 @@ run_model <- T
 
 if(run_model) {
   
-<<<<<<< HEAD
-  all_votes <- readr::read_csv('data/parliament_observatory_al_bawsala/Votes_M01.csv')
-  
-  all_votes <-  mutate(all_votes,mp_bloc_name=recode(mp_bloc_name,
-                                                     `Afek Tounes et l'appel des tunisiens à l'étranger`="Afek Tounes",
-                                                     `Bloc Social-Démocrate`="Social-Démocrate",
-                                                     `Bloc Al Horra du Mouvement Machrouu Tounes`="Horra",
-                                                     `Mouvement Ennahdha`="Nahda",
-                                                     `Mouvement Nidaa Tounes`='Nidaa Tounes',
-                                                     `Coalition nationale`="Tahya Tounes"),
-                       vote_title=coalesce(vote_title,as.character(vote_date)),
-                       vote_choice=factor(vote_choice,levels=c("NO","ABSTAIN","YES")),
-                       change=as.numeric(vote_date>ymd("2016-07-13")),
-                       ordered_id=3)
-  
-  unam_votes <- group_by(all_votes, vote_id) %>% 
-    summarize(unan=all(vote_choice[!is.na(vote_choice)]=="YES") || all(vote_choice[!is.na(vote_choice)]=="NO"))
-  
-  all_votes <- anti_join(all_votes,filter(unam_votes,unan),by="vote_id")
-  
-  # to run this model, need to remove those parties over which the covariate is not defined
-  all_votes <- filter(all_votes,mp_bloc_name %in% c('Nahda',
-                                                    'Nidaa Tounes',
-                                                    'Front Populaire',
-                                                    "Social-Démocrate",
-                                                    "Afek Tounes",
-                                                    "Tahya Tounes",
-                                                    "Horra",
-                                                    "Union Patriotique Libre",
-                                                    "Bloc Démocrate",
-                                                    "Aucun bloc")) %>% 
-    mutate(mp_bloc_name=recode(mp_bloc_name,
-                               `Bloc Démocrate`="Social-Démocrate",
-                               `Tahya Tounes`="Nidaa Tounes"))
-  
-  bill_stat <- all_votes %>% 
-    group_by(vote_id) %>% 
-    count(vote_choice)
-  
-  bill_stat_nahda <- filter(all_votes, mp_bloc_name=="Nahda") %>% 
-    group_by(vote_id) %>% 
-    count(vote_choice,.drop=F)
-  
-  bill_stat_fp <- filter(all_votes, mp_bloc_name %in% c("Nidaa Tounes", "Tahya Tounes","Horra")) %>% 
-    group_by(vote_id) %>% 
-    count(vote_choice,.drop=F)
-  
-  bill_stat <- left_join(bill_stat_fp,bill_stat_nahda, by=c("vote_id","vote_choice")) %>% 
-    filter(!is.na(vote_choice))
-  
-  bill_stat_yes <- filter(bill_stat, vote_choice=="YES") %>% 
-    mutate(vote_diff=sqrt((n.x - n.y)^2))
-  bill_stat_no <- filter(bill_stat, vote_choice=="NO") %>% 
-    mutate(vote_diff=sqrt((n.x - n.y)^2))
-=======
+  # all_votes <- readr::read_csv('data/parliament_observatory_al_bawsala/Votes_M01.csv')
+  # 
+  # all_votes <-  mutate(all_votes,mp_bloc_name=recode(mp_bloc_name,
+  #                                                    `Afek Tounes et l'appel des tunisiens à l'étranger`="Afek Tounes",
+  #                                                    `Bloc Social-Démocrate`="Social-Démocrate",
+  #                                                    `Bloc Al Horra du Mouvement Machrouu Tounes`="Horra",
+  #                                                    `Mouvement Ennahdha`="Nahda",
+  #                                                    `Mouvement Nidaa Tounes`='Nidaa Tounes',
+  #                                                    `Coalition nationale`="Tahya Tounes"),
+  #                      vote_title=coalesce(vote_title,as.character(vote_date)),
+  #                      vote_choice=factor(vote_choice,levels=c("NO","ABSTAIN","YES")),
+  #                      change=as.numeric(vote_date>ymd("2016-07-13")),
+  #                      ordered_id=3)
+  # 
+  # unam_votes <- group_by(all_votes, vote_id) %>% 
+  #   summarize(unan=all(vote_choice[!is.na(vote_choice)]=="YES") || all(vote_choice[!is.na(vote_choice)]=="NO"))
+  # 
+  # all_votes <- anti_join(all_votes,filter(unam_votes,unan),by="vote_id")
+  # 
+  # # to run this model, need to remove those parties over which the covariate is not defined
+  # all_votes <- filter(all_votes,mp_bloc_name %in% c('Nahda',
+  #                                                   'Nidaa Tounes',
+  #                                                   'Front Populaire',
+  #                                                   "Social-Démocrate",
+  #                                                   "Afek Tounes",
+  #                                                   "Tahya Tounes",
+  #                                                   "Horra",
+  #                                                   "Union Patriotique Libre",
+  #                                                   "Bloc Démocrate",
+  #                                                   "Aucun bloc")) %>% 
+  #   mutate(mp_bloc_name=recode(mp_bloc_name,
+  #                              `Bloc Démocrate`="Social-Démocrate",
+  #                              `Tahya Tounes`="Nidaa Tounes"))
+  # 
+  # bill_stat <- all_votes %>% 
+  #   group_by(vote_id) %>% 
+  #   count(vote_choice)
+  # 
+  # bill_stat_nahda <- filter(all_votes, mp_bloc_name=="Nahda") %>% 
+  #   group_by(vote_id) %>% 
+  #   count(vote_choice,.drop=F)
+  # 
+  # bill_stat_fp <- filter(all_votes, mp_bloc_name %in% c("Nidaa Tounes", "Tahya Tounes","Horra")) %>% 
+  #   group_by(vote_id) %>% 
+  #   count(vote_choice,.drop=F)
+  # 
+  # bill_stat <- left_join(bill_stat_fp,bill_stat_nahda, by=c("vote_id","vote_choice")) %>% 
+  #   filter(!is.na(vote_choice))
+  # 
+  # bill_stat_yes <- filter(bill_stat, vote_choice=="YES") %>% 
+  #   mutate(vote_diff=sqrt((n.x - n.y)^2))
+  # bill_stat_no <- filter(bill_stat, vote_choice=="NO") %>% 
+  #   mutate(vote_diff=sqrt((n.x - n.y)^2))
+
   # use script create_data.R to create the rds file
->>>>>>> 7b61294baf3756b39467fc4747db62e20424c06f
   
   all_votes <- readRDS("data/all_votes.rds")
   
@@ -111,43 +109,31 @@ if(run_model) {
                             group_id="mp_bloc_name",remove_cov_int = F,
                             person_cov=~change*mp_bloc_name)
   
-<<<<<<< HEAD
-  arp_ideal_data@person_cov <- c(arp_ideal_data@person_cov[1],arp_ideal_data@person_cov[10:15])
-  arp_ideal_data@score_matrix <- select(arp_ideal_data@score_matrix,item_id:change,
-                                        `change:mp_bloc_nameAucun bloc`:discrete)
-=======
-  # arp_ideal_data@person_cov <- c(arp_ideal_data@person_cov[1],arp_ideal_data@person_cov[10:17])
+  # arp_ideal_data@person_cov <- c(arp_ideal_data@person_cov[1],arp_ideal_data@person_cov[10:15])
   # arp_ideal_data@score_matrix <- select(arp_ideal_data@score_matrix,item_id:change,
   #                                       `change:mp_bloc_nameAucun bloc`:discrete)
->>>>>>> 7b61294baf3756b39467fc4747db62e20424c06f
+
+  # arp_ideal_data@person_cov <- c(arp_ideal_data@person_cov[1],arp_ideal_data@person_cov[10:17])
+  # arp_ideal_data@score_matrix <- select(arp_ideal_data@score_matrix,item_id:change,
   
   
   estimate_all <- id_estimate(arp_ideal_data,
                               use_groups = F,
                               restrict_ind_high="5aeae5b84f24d02328a2f1aa",
                               restrict_ind_low="58528919cf44121f3e63aee5",
-<<<<<<< HEAD
                               #restrict_ind_high="57a31e71cf44122088ceed2c",
                               #restrict_ind_low="57a31e71cf44122088ceed31",
-                              const_type="items",time_var=12000,
+                              const_type="items",
                               #restrict_ind_high= "Nahda",
                               #restrict_ind_low="Front Populaire",
-                              model_type=3,
-                              vary_ideal_pts = 'random_walk',nchains = 2,
+                              model_type=3,map_over_id = "persons",
+                              vary_ideal_pts = 'splines',
+                              spline_degree=3,
+                              nchains = 2,
                               ncores = parallel::detectCores(),
                               fixtype='prefix',niters = 250,
                               warmup=250,id_refresh=10)
-=======
-                              const_type="items",time_var=100,restrict_var = F,
-                              niters=250,warmup=250,time_fix_sd = .01,
-                              #restrict_ind_high= "Nahda",
-                              #restrict_ind_low="Front Populaire",
-                              model_type=4,save_files="/scratch/rmk7/arp/junk/",
-                              vary_ideal_pts = 'random_walk',nchains = 1,
-                              ncores = parallel::detectCores(),max_treedepth=10,
-                              fixtype='prefix',id_refresh=10)
->>>>>>> 7b61294baf3756b39467fc4747db62e20424c06f
-  
+   
   
   
   saveRDS(estimate_all,'/scratch/rmk7/arp/estimate_all_ar3_full_rv.rds')
